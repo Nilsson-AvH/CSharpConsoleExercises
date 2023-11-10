254bceb6aa02019adf706d4ef7226358a98665fb
NilssonLoRi <nlopez153@misena.edu.co>
2023-11-10 17:37:29 -0500

    First commit

---
+++ 001_invertirNumeroDosCifras/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.CompilerServices;
+
+namespace invertirNumeroDosCifras
+{
+    class InvertirNumeros
+    {
+        // Constructors
+        
+        // Attribs
+        private int AUX, DEC, UNI;
+
+        // Getter and Setter methods
+
+        // Methods
+        public void Invertir(int numeroAInvertir)
+        {
+            DEC = numeroAInvertir/10;
+            UNI = numeroAInvertir%10;
+            AUX = (UNI * 10) + DEC;
+
+            Console.WriteLine( "El numero " +numeroAInvertir+ " invertido es: " + AUX );
+        }
+    }
+    class Program
+    {
+        // Constructors
+
+        // Attribs
+
+        // Getter and Setter methods
+
+        // Methods
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Ingrese un numero de dos cifras:");
+            InvertirNumeros elNumero = new InvertirNumeros();
+            try
+            {
+                input=int.Parse(Console.ReadLine());
+                if (input >= 10 && input <= 99) elNumero.Invertir(input);
+                else Console.WriteLine("Inserte un numero de dos cifras.");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Caracteres invalidos: " + ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ 002_inverturNumeroTresCifras/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace invTresCifras
+{
+    class InvTresCifras
+    {
+        // Constructor
+
+        // Attribs
+        private int CEN, DEC, UNI, NUM; 
+
+        // Getter and setter methods
+
+        // Methods
+        public int superInversor(int numeroAInvertir)
+        {
+            NUM = numeroAInvertir;
+            CEN = NUM / 100;
+            NUM = NUM % 100;
+            DEC = NUM / 10;
+            UNI = NUM % 10;
+
+            return (UNI * 100) + (DEC * 10) + CEN;
+        }
+    }
+    class Program
+    {
+        // Constructor
+
+        // Attribs
+
+        // Gettter and Setter methods
+
+        // Methods
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa inversor de numeros:");
+            Console.WriteLine("Digite un numero de tres digitos");
+            
+            InvTresCifras input = new InvTresCifras();
+
+            Console.WriteLine("El numero inverso es: " + input.superInversor(int.Parse(Console.ReadLine())));
+        }
+    }
+}
